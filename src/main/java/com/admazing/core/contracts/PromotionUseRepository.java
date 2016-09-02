a76305a7510b90e8185abb06dd752d1149da5a1c
Mi Pc <Mi Pc@PC>
2016-09-02 00:03:11 -0500

    Create save function in PromotionUseRepository

---
+++ src/main/java/com/admazing/core/contracts/PromotionUseRepository.java
@@ -0,0 +1,6 @@
+package com.admazing.core.contracts;
+
+
+public interface PromotionUseRepository {
+	boolean save(String idAccess,String idPromotion);
+}
---
+++ src/main/java/com/admazing/dataAccess/PromotionUseRepositoryImpl.java
@@ -0,0 +1,75 @@
+package com.admazing.dataAccess;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+
+import com.admazing.PromotionUseModel;
+import com.admazing.core.contracts.PromotionUseRepository;
+
+public class PromotionUseRepositoryImpl implements PromotionUseRepository{
+	
+	@Override
+	public boolean save(String idAccess, String idPromotion) {
+		
+		boolean success=false;
+		Session session=hibernateUtil.getSessionFactory().openSession();
+		Transaction transaction=session.beginTransaction();
+		try{
+			PromotionUseModel lastPromotionUse = null;
+			Criteria cr = session.createCriteria(PromotionUseModel.class);
+			cr.addOrder(Order.desc("idUse"));
+			cr.setMaxResults(1);
+			List<PromotionUseModel> promotionsUse = new ArrayList<PromotionUseModel>(); 
+			promotionsUse=cr.list();
+			for (PromotionUseModel promotionUse:promotionsUse){
+				lastPromotionUse= promotionUse;
+			}
+			
+			String idCurrentPromotionUse;
+			if(lastPromotionUse!=null){
+				idCurrentPromotionUse=getNextIdPromotionUse(lastPromotionUse.getIdUse());
+			}
+			else{
+				idCurrentPromotionUse="PU000001";
+			}
+			PromotionUseModel currentPromotionUse=fillPromotionUse(idCurrentPromotionUse,idAccess ,idPromotion);
+		    session.save(currentPromotionUse);
+		    session.getTransaction().commit();		
+	        session.flush();
+	        session.clear();
+	        success=true;
+			
+		} catch (Exception e) {
+	            e.printStackTrace();
+	            transaction.rollback();
+		} finally {
+			if(session.isOpen())
+				session.close();
+	    }
+			
+	return success;
+	}
+	
+	private PromotionUseModel fillPromotionUse(String idUse, String idAccess, String idPromotion ){
+		PromotionUseModel currentPromotionUse = new PromotionUseModel();
+		currentPromotionUse.setIdUse(idUse);
+		currentPromotionUse.setIdAccess(idAccess);
+		currentPromotionUse.setIdPromotion(idPromotion);
+		return currentPromotionUse;
+	}
+	
+	private String getNextIdPromotionUse(String idLastPromotionUse){
+		Integer idPromotionUseCalculated=Integer.parseInt(idLastPromotionUse.substring(2))+1;
+		String zeros=repeat("0", 6-(int)(Math.log10(idPromotionUseCalculated)+1));
+		String idCurrenPromotionUse="PU"+zeros+String.valueOf(idPromotionUseCalculated);
+		return idCurrenPromotionUse;
+	}
+	private static String repeat(String str, int times) {
+	    return new String(new char[times]).replace("\0", str);
+	}
+
+}
