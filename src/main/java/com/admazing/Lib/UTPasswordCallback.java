75e6f1aeaa41bf5b4db949e1f775e8a6ea8615d6
Mi Pc <Mi Pc@PC>
2016-09-12 16:11:51 -0500

    Change Authentication to authenticate with username and passwords form the database

--- src/main/java/com/admazing/Lib/UTPasswordCallback.java
+++ src/main/java/com/admazing/Lib/UTPasswordCallback.java
@@ -11,26 +11,27 @@ import javax.security.auth.callback.UnsupportedCallbackException;
 
 import org.apache.wss4j.common.ext.WSPasswordCallback;
 
+import com.admazing.UserModel;
+import com.admazing.core.contracts.UserRepository;
+import com.admazing.dataAccess.UserRepositoryImpl;
+
 
 
 public class UTPasswordCallback implements CallbackHandler {
 
-	private Map<String, String> passwords = new HashMap<String, String>();
-
+	UserRepository userRepository= new UserRepositoryImpl();
 	public UTPasswordCallback() {
 		
-		passwords.put("Bharath", "Bharath");
-		passwords.put("cxf", "cxf");
 	}
 
 	public void handle(Callback[] callbacks) throws IOException,
 			UnsupportedCallbackException {
 		for (int i = 0; i < callbacks.length; i++) {
 			WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
-
-			String pass = passwords.get(pc.getIdentifier());
-			if (pass != null) {
-				pc.setPassword(pass);
+			UserModel user=null;
+			user=userRepository.findById(pc.getIdentifier());
+			if(user!=null){
+				pc.setPassword(user.getPassword());
 				return;
 			}
 		}
