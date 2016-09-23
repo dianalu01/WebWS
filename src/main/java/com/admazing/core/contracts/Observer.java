6b1ae180854297d1932b3fc5ade311f16c45ba5e
Mi Pc <Mi Pc@PC>
2016-09-23 18:55:05 -0500

    Try fix error

--- src/main/java/com/admazing/core/contracts/Observer.java
+++ src/main/java/com/admazing/core/contracts/Observer.java
@@ -3,7 +3,7 @@ package com.admazing.core.contracts;
 import com.admazing.core.contracts.PromotionUseService;
 
 public abstract class Observer {
-	protected PromotionUseService subject;
+	protected static PromotionUseService subject;
 	public abstract void update(String idUser,String idPromotion);
 
 }
