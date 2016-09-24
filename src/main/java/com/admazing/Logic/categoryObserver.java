5a8f43ad79fe4702e9ee886f93886dffd9c3a2a6
Mi Pc <Mi Pc@PC>
2016-09-23 19:33:19 -0500

    Fixed Error in Observer

--- src/main/java/com/admazing/Logic/categoryObserver.java
+++ src/main/java/com/admazing/Logic/categoryObserver.java
@@ -5,15 +5,9 @@ import java.util.List;
 import com.admazing.PromotionModel;
 import com.admazing.PromotionUseModel;
 import com.admazing.core.contracts.Observer;
-import com.admazing.core.contracts.PreferenceRepository;
 import com.admazing.core.contracts.PreferenceService;
-import com.admazing.core.contracts.PromotionRepository;
 import com.admazing.core.contracts.PromotionService;
-import com.admazing.core.contracts.PromotionUseRepository;
 import com.admazing.core.contracts.PromotionUseService;
-import com.admazing.dataAccess.PreferenceRepositoryImpl;
-import com.admazing.dataAccess.PromotionRepositoryImpl;
-import com.admazing.dataAccess.PromotionUseRepositoryImpl;
 
 public class categoryObserver extends Observer{
 	
