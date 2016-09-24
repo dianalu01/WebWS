db3c31ba6f812c3656fa621f6625ae7fe2bb200d
Mi Pc <Mi Pc@PC>
2016-09-23 19:16:36 -0500

    Try fix error

--- src/main/java/com/admazing/Logic/categoryObserver.java
+++ src/main/java/com/admazing/Logic/categoryObserver.java
@@ -5,44 +5,50 @@ import java.util.List;
 import com.admazing.PromotionModel;
 import com.admazing.PromotionUseModel;
 import com.admazing.core.contracts.Observer;
-import com.admazing.core.contracts.PreferenceService;
-import com.admazing.core.contracts.PromotionService;
+import com.admazing.core.contracts.PreferenceRepository;
+import com.admazing.core.contracts.PromotionRepository;
+import com.admazing.core.contracts.PromotionUseRepository;
 import com.admazing.core.contracts.PromotionUseService;
+import com.admazing.dataAccess.PreferenceRepositoryImpl;
+import com.admazing.dataAccess.PromotionRepositoryImpl;
+import com.admazing.dataAccess.PromotionUseRepositoryImpl;
+
 public class categoryObserver extends Observer{
 	
-	private PreferenceService preferenceService;
-	private PromotionService promotionService;
-	private PromotionUseService promotionUseService;
+	private PreferenceRepository preferenceRepository;
+	private PromotionRepository promotionRepository;
+	private PromotionUseRepository promotionUseRepository;
 	
 	public categoryObserver(PromotionUseService subject){
-		preferenceService = new PreferenceServiceImpl();
-		promotionService= new PromotionServiceImpl();
-		promotionUseService = new PromotionUseServiceImpl();
+		preferenceRepository = new PreferenceRepositoryImpl();
+		promotionRepository= new PromotionRepositoryImpl();
+		promotionUseRepository = new PromotionUseRepositoryImpl();
 			
 		Observer.subject=subject;
-		Observer.subject.attach(this);
+		this.subject.attach(this);
 	}
 
 	@Override
 	public void update(String idUser,String idPromotion) {
-		PromotionModel promotionUsed=promotionService.findById(idPromotion);
-		if(!preferenceService.exist(idUser, promotionUsed.getIdCategory())){		
+		PromotionModel promotionUsed=promotionRepository.findById(idPromotion);
+		if(!preferenceRepository.exist(idUser, promotionUsed.getIdCategory())){		
 			if(countPromotionsUsed(idUser,promotionUsed.getIdCategory())>=5){
-				preferenceService.save(idUser,promotionUsed.getIdCategory());				
+				preferenceRepository.save(idUser,promotionUsed.getIdCategory());				
 			}				
 		}				
 	}
 	
 	private int countPromotionsUsed (String idUser, String idCategory){
 		int nPromotionUsed=0;
-		List<PromotionUseModel> promotionsUse = promotionUseService.getById(idUser);
+		List<PromotionUseModel> promotionsUse = promotionUseRepository.findById(idUser);
 		for(PromotionUseModel promotionUse : promotionsUse){
-			PromotionModel promotion=promotionService.findById(promotionUse.getIdPromotion());
+			PromotionModel promotion=promotionRepository.findById(promotionUse.getIdPromotion());
 			if(promotion.getIdCategory().compareTo(idCategory)==0){
 				nPromotionUsed++;
 			}
 		}
 		return nPromotionUsed;
 	}
+
 	
 }
